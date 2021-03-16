4d327448953dd0787a6b56ad6f52764a130588fd
villares <brauliovtorres@gmail.com>
2021-03-16 00:46:17 -0300

    Criação do código fonte inicial do projeto BACK-END.

---
+++ FONTES/BACK-END/sapros/src/main/java/br/com/sapros/SaprosApplication.java
@@ -0,0 +1,13 @@
+package br.com.sapros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaprosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaprosApplication.class, args);
+	}
+
+}
